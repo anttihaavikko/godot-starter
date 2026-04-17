58658166772a0cf8cadece9b1421178e63ce07a3
Antti Haavikko <antti.haavikko@gmail.com>
2026-04-17 15:12:01 +0300

    Add possible delay to appearer

--- _Starter/Scripts/appearer.gd
+++ _Starter/Scripts/appearer.gd
@@ -8,6 +8,7 @@ extends Node
 var obj
 var size: Vector2
 var shown := false
+var tween: PropertyTweener
 
 func _ready() -> void:
 	obj = self
@@ -16,23 +17,27 @@ func _ready() -> void:
 		obj.scale = Vector2.ZERO
 		
 	if appear_after > 0:
-		await get_tree().create_timer(appear_after).timeout
-		appear()
+		appear(appear_after)
 	if auto_hide_after:
-		await get_tree().create_timer(auto_hide_after).timeout
-		disappear()
+		disappear(auto_hide_after)
 
-func appear():
+func appear(delay: float = 0.0):
 	if shown or not get_tree():
 		return
+	if tween: await tween.finished
 	shown = true
-	get_tree().create_tween().tween_property(obj, "scale", size, duration).set_trans(Tween.TRANS_BOUNCE)
+	tween = get_tree().create_tween().tween_property(obj, "scale", size, duration).set_trans(Tween.TRANS_BOUNCE).set_delay(delay)
+	await tween.finished
+	tween = null
 
-func disappear():
+func disappear(delay: float = 0.0):
 	if not shown or not get_tree():
 		return
+	if tween: await tween.finished
 	shown = false
-	get_tree().create_tween().tween_property(obj, "scale", Vector2.ZERO, duration).set_trans(Tween.TRANS_ELASTIC)
+	tween = get_tree().create_tween().tween_property(obj, "scale", Vector2.ZERO, duration).set_trans(Tween.TRANS_ELASTIC).set_delay(delay)
+	await tween.finished
+	tween = null
 
 func show_with_text(text: String):
 	if obj is Label or obj is RichTextLabel:
